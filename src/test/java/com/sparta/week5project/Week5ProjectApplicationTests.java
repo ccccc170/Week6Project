3bfc604b390c45bb12bb72e42ced506e82ba9474
Abdullah <abdullahnazeer22@gmail.com>
2022-12-20 12:28:13 +0000

    first-commit

---
+++ src/test/java/com/sparta/week5project/Week5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week5project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
