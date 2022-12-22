938f667aed8f8a37c1ceed6a21088816e7c03244
CameronASHiggins <74916249+CameronASHiggins@users.noreply.github.com>
2022-12-22 14:22:55 +0000

    Added delete by id test in department manager DAO test

--- src/test/java/com/sparta/week5project/DAO/impl/DepartmentManagerDAOTest.java
+++ src/test/java/com/sparta/week5project/DAO/impl/DepartmentManagerDAOTest.java
@@ -2,12 +2,14 @@ package com.sparta.week5project.DAO.impl;
 
 import com.sparta.week5project.DTO.DeptManagerDTO;
 import com.sparta.week5project.entities.DeptManagerId;
+import com.sparta.week5project.repositories.DepartmentRepository;
 import com.sparta.week5project.repositories.DeptManagerRepository;
 import com.sparta.week5project.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import java.util.Optional;
 
@@ -21,6 +23,8 @@ class DepartmentManagerDAOTest {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private DepartmentRepository departmentRepository;
 
     @Test
     void findByIdTest() {
@@ -37,6 +41,12 @@ class DepartmentManagerDAOTest {
     }
 
     @Test
-    void deleteBYIdTest() {
+    @Rollback
+    void deleteByIdTest() {
+        DeptManagerId deptManagerId = new DeptManagerId();
+        deptManagerId.setEmpNo(110022);
+        deptManagerId.setDeptNo("d001");
+        departmentManagerDAO.deleteById(deptManagerId);
+        Assertions.assertFalse(deptManagerRepository.findById(deptManagerId).isPresent());
     }
 }
