e56237c03f07b7ecdf21893352fc32d045306e16
patrykblakala <patrykblakala108@gmail.com>
2022-12-22 14:28:01 +0000

    Creating more tests for DAO classes

--- src/test/java/com/sparta/week5project/DAO/impl/DepartmentManagerDAOTest.java
+++ src/test/java/com/sparta/week5project/DAO/impl/DepartmentManagerDAOTest.java
@@ -1,7 +1,11 @@
 package com.sparta.week5project.DAO.impl;
 
 import com.sparta.week5project.DTO.DeptManagerDTO;
+import com.sparta.week5project.entities.Department;
 import com.sparta.week5project.entities.DeptManagerId;
+import com.sparta.week5project.entities.Employee;
+import com.sparta.week5project.mappers.impl.DeptManagerMapperImpl;
+import com.sparta.week5project.repositories.DepartmentRepository;
 import com.sparta.week5project.repositories.DeptManagerRepository;
 import com.sparta.week5project.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
@@ -9,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @SpringBootTest
@@ -21,6 +26,11 @@ class DepartmentManagerDAOTest {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private DeptManagerMapperImpl deptManagerMapper;
 
     @Test
     void findByIdTest() {
@@ -33,10 +43,32 @@ class DepartmentManagerDAOTest {
     }
 
     @Test
+    void saveTest() {
+        Optional<Employee> employee = employeeRepository.findById(10001);
+
+        DeptManagerId deptManagerId = new DeptManagerId();
+    deptManagerId.setEmpNo(100000);
+    deptManagerId.setDeptNo("Staff");
+        Department department = new Department();
+        department.setDeptName("Staff");
+        department.setId("d009");
+    DeptManagerDTO deptManagerDTO = new DeptManagerDTO();
+    deptManagerDTO.setToDate(LocalDate.of(2022, 1,1));
+    deptManagerDTO.setFromDate(LocalDate.of(1999,5,5));
+    deptManagerDTO.setDeptNo(department);
+    deptManagerDTO.setId(deptManagerId);
+    deptManagerDTO.setEmpNo(employee.get());
+    departmentManagerDAO.save(deptManagerDTO);
+    Assertions.assertTrue(departmentManagerDAO.findById(deptManagerId).isPresent());
+    }
+    @Test
     void updateTest() {
+
     }
 
     @Test
     void deleteBYIdTest() {
     }
+
+
 }
